4dd27f32a4b3f34439710978fd99a9dda7774cbc
cactuaroid <solamiredoredore@gmail.com>
2016-07-20 01:46:50 -0700

    sample of DI by MEF

---
+++ MefSample.Logging/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Runtime.CompilerServices;
+
+namespace MefSample.Logging
+{
+    [Export]
+    public class Logger
+    {
+        public void Info(string content, [CallerMemberName]string caller = "")
+        {
+            Write("Info: " + caller + ": " + content);
+        }
+
+        public void Error(string content, [CallerMemberName]string caller = "")
+        {
+            Write("Error: " + caller + ": " + content);
+        }
+
+        private void Write(string content)
+        {
+            Console.WriteLine(DateTime.Now + " " + content);
+        }
+    }
+}
